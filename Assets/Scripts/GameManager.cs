91b26014f9b80df3a2faa6e5adfa9d38a52c2602
j1sung <dalssagi00@gmail.com>
2024-11-01 08:10:36 +0900

    ff

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,17 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
 
     private bool isClear;
+    private bool isGameOver;
 
-    public void SetClear() // ½Â¸® ¼³Á¤
-    {
-        isClear = true;
-    }
+    public delegate void GameStatusChange();
+    public event GameStatusChange OnGameClear;
+    public event GameStatusChange OnGameOver;
 
     private void Awake()
     {
@@ -19,16 +21,47 @@ public class GameManager : MonoBehaviour
         {
             instance = this;
         }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
     void Start()
     {
         isClear = false;
+        isGameOver = false;
     }
-
 
     void Update()
     {
-        
+        /*if (Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene(1);
+        }*/
+
+        if (isGameOver)
+        {
+
+        }
+    }
+    public void SetClear()
+    {
+        if (!isClear)
+        {
+            isClear = true;
+            OnGameClear?.Invoke();
+            Debug.Log("clear");
+        }
+    }
+
+    public void SetGameOver()
+    {
+        if (!isGameOver)
+        {
+            isGameOver = true;
+            OnGameOver?.Invoke();
+            Debug.Log("over");
+        }
     }
 }
